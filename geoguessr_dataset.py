86c245aaa6ff3baae1ddea5e334cf2471be67528
Weizhen Zhou <wz3008@cm010.hpc.nyu.edu>
2025-11-16 19:56:42 -0500

    X

--- geoguessr_dataset.py
+++ geoguessr_dataset.py
@@ -8,36 +8,36 @@ from pathlib import Path
 import csv
 import glob
 
-# class GeoGuessrDataset(Dataset):
-#     def __init__(self, data_dir):
-#         self.data_dir = data_dir
-#         self.targets = np.load(os.path.join(data_dir, 'targets.npy'), allow_pickle=True)
+class GeoGuessrDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.targets = np.load(os.path.join(data_dir, 'targets.npy'), allow_pickle=True)
 
-#     def __len__(self):
-#         return len(os.listdir(self.data_dir)) - 1
+    def __len__(self):
+        return len(os.listdir(self.data_dir)) - 1
 
-#     def __getitem__(self, idx):
-#         data_path = os.path.join(self.data_dir, f'street_view_{idx}.jpg')
+    def __getitem__(self, idx):
+        data_path = os.path.join(self.data_dir, f'street_view_{idx}.jpg')
         
-#         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-#                                     std=[0.229, 0.224, 0.225])
-#         transform = transforms.Compose([
-#                 transforms.Resize(256),
-#                 transforms.ToTensor(),
-#                 normalize,
-#             ])
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+        transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.ToTensor(),
+                normalize,
+            ])
         
-#         img = pil_loader(data_path)
-#         data = transform(img)
+        img = pil_loader(data_path)
+        data = transform(img)
         
-#         target = torch.tensor(self.targets[idx], dtype=torch.float)
+        target = torch.tensor(self.targets[idx], dtype=torch.float)
         
-#         return data, target
+        return data, target
     
-# def pil_loader(path: str) -> Image.Image:
-#     with open(path, "rb") as f:
-#         img = Image.open(f)
-#         return img.convert("RGB")
+def pil_loader(path: str) -> Image.Image:
+    with open(path, "rb") as f:
+        img = Image.open(f)
+        return img.convert("RGB")
     
 
 class GeoGuessrDataset(Dataset):
